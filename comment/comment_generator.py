075cac5de747e316ea73ff189903806fc061a14a
wangxiaocheng <wangxiaocheng.gpt@bytedance.com>
2023-06-08 08:28:21 +0800

    add git ignore

--- comment/comment_generator.py
+++ comment/comment_generator.py
@@ -1,34 +1,53 @@
 import os
+import time
+from tqdm import tqdm
+from claude.claude import ClaudeClient
+from langchain.text_splitter import (
+    RecursiveCharacterTextSplitter,
+    Language,
+)
+
+client = ClaudeClient()
 
 
-def traverse_files(dir_path):
+def _default_file_function(file_path):
+    print(f"size of file {file_path}: {os.path.getsize(file_path)}")
+
+
+def _default_dir_function(dir_path):
+    # print(f"size of dir {dir_path}: {os.stat(dir_path).st_size}")
+    pass
+
+def traverse_files(dir_path, file_function, dir_function):
+    if file_function is None:
+        file_function = _default_file_function
+    if dir_function is None:
+        dir_function = _default_dir_function
+    print(dir_path)
     if dir_path.startswith("."):
         dir_path = os.path.abspath(dir_path)
     re = []
+    if not os.path.isdir(dir_path):
+        file_function(dir_path)
+        return re
+
     files = os.listdir(dir_path)
-    for file in files:
-        if file.startswith("__pycache__") or file.startswith("."):
+    for file in tqdm(files):
+        if file.startswith("__pycache__") or file.startswith(".") or file.startswith("target"):
             continue
         full_path = os.path.join(dir_path, file)
         if os.path.isdir(full_path) and not file.startswith('.'):
-            re.append(full_path)
-            re.append(traverse_files(full_path))
+            dir_function(full_path)
+            traverse_files(full_path,file_function,dir_function)
         elif not file.startswith('.'):
-            re.append(full_path)
+            file_function(full_path)
     return re
-
-
-import os
-import time
-
-import os
-import time
 
 
 def create_obsidian_page(name, content):
     """创建一个Obsidian页面"""
     # 检查文件夹是否存在,不存在则创建
-    folder_path = os.environ.get("OBVAULTPATH","/Users/bytedance/Documents/Obsidian Vault")
+    folder_path = os.environ.get("OBVAULTPATH", "/Users/bytedance/Documents/test_for_ob/test_for_llm")
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
 
@@ -49,11 +68,62 @@ def create_obsidian_page(name, content):
     print("页面更新成功!")
 
 
-def create_hook(file_path,):
+def create_hook(file_path, ):
     pass
 
+
+language_mapper = {
+    "cpp": Language.CPP,
+    "go": Language.GO,
+    "java": Language.JAVA,
+    "js": Language.JS,
+    "php": Language.PHP,
+    "proto": Language.PROTO,
+    "python": Language.PYTHON,
+    "rst": Language.RST,
+    "ruby": Language.RUBY,
+    "rust": Language.RUST,
+    "scala": Language.SCALA,
+    "swift": Language.SWIFT,
+    "latex": Language.LATEX,
+    "html": Language.HTML
+}
+
+
+def fake_file_function_code_explain(file_path: os.path):
+    prompt = "解释上述代码，并用markdown格式输出"
+    file_type = os.path.splitext(file_path)[1][1:]
+    file_type_lang = language_mapper.get(file_type)
+    if file_type_lang is None:
+        return
+    else:
+        print(file_path)
+
+
+def file_function_code_explain(file_path: os.path):
+    prompt = "解释上述代码，并用markdown格式输出"
+    file_type = os.path.splitext(file_path)[1][1:]
+    file_type_lang = language_mapper.get(file_type)
+    if file_type_lang is None:
+        return
+    with open(file_path, "r", encoding="utf-8") as f:
+        content = f.read()
+
+        code_splitter = RecursiveCharacterTextSplitter.from_language(
+            language=file_type_lang, chunk_size=7000, chunk_overlap=1000
+        )
+        code_doc = code_splitter.create_documents([content])
+        content = []
+        for code_block in code_doc:
+            result = client.chat(code_block.page_content + "\n" + prompt)
+            parse_result = result.replace("\\n", "\n")
+            content.append(parse_result[1: -1])
+        print(f"finish for file {file_path}\n")
+        file_result = "\n\n\n".join(content)
+        obsidian_name = file_path.split("/")[-1]
+        create_obsidian_page(obsidian_name, file_result)
+
+
 if __name__ == '__main__':
-    import time
-    create_obsidian_page("thisIsTest","aaa")
-    time.sleep(2)
-    create_obsidian_page("thisIsTest","bbb")
+    traverse_files("/Users/bytedance/workspace/src/cromwell_bd/cromwell/common/src/main/scala/common", file_function=file_function_code_explain,
+                   dir_function=_default_dir_function)
--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-# This is a sample Python script.
+# import pandas as pd
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+#
+# # 统计每个ShortName和Year出现的Name数量
+# counts = df.groupby(['ShortName', 'Year'])['Name'].nunique().reset_index()
+#
+#
+# # 将结果写入Excel
+# writer = pd.ExcelWriter('./results.xlsx')
+# counts.to_excel(writer, sheet_name='results', index=False)
+# writer._save()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+import pandas as pd
 
+df = pd.read_excel('/Users/bytedance/Downloads/test.xlsx', sheet_name=0)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+result = []
+for short_name in df['ShortName'].unique():
+    for year in df['Year'].unique():
+        try:
+            name = df[(df['ShortName'] == short_name) & (df['Year'] == year)]['Name'].iloc[0]
+            count = df[(df['ShortName'] == short_name) & (df['Year'] == year)]['Name'].count()
+            result.append([short_name, year, name, count])
+        except Exception as e:
+            pass
+            # print(df['Year'], e)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+result_df = pd.DataFrame(result, columns=['ShortName', 'Year', 'name', 'count'])
+
+writer = pd.ExcelWriter('./result12.xlsx')
+result_df.to_excel(writer, 'Sheet1')
+writer._save()
